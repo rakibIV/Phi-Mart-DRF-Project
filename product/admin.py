b21a74318559a17b9e07ac8e5905653252e7485e
rakibIV <rakibhasan42008@gmail.com>
2025-06-02 10:19:13 +0600

    Product models added in admin and is_staff added in response

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from product.models import Product, Category, ProductImage
 
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','price','stock')
+# admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(ProductImage)
