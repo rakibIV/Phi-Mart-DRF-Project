f28586b17effaa76598af7625978596fa1079213
rakibIV <rakibhasan42008@gmail.com>
2025-06-03 17:22:06 +0600

    Vercel cors setup done

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -175,7 +175,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS =[
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://phimart-client-sigma.vercel.app'
 ]
 
 
